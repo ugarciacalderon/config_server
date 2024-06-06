be150c56373326237c323c0efeef3d80928f4309
Ulises Garcia <ugarciacalderon@gmail.com>
2024-06-05 19:54:50 -0600

    fix(added exclude DataSource) in SpringBootApplication
    to connect databse in aws located

--- src/main/java/com/ugarciac/configserver/ConfigserverApplication.java
+++ src/main/java/com/ugarciac/configserver/ConfigserverApplication.java
@@ -2,9 +2,10 @@ package com.ugarciac.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableConfigServer
 public class ConfigserverApplication {
 
